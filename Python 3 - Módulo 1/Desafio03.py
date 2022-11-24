1ba3b1b6b11b0d09ebb4dd7ab8bbf6d28797601c
Luan Tobias <luantmd50@gmail.com>
2022-11-23 21:01:15 -0300

    Primeiros passos com Python
    Os desafios 01 ao 04 fazem parte das sete primeiras aulas de introdução ao Python.

---
+++ Python 3 - Módulo 1/Desafio03.py
@@ -0,0 +1,4 @@
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+resultado = int(numero1)+int(numero2)
+print ('A soma dos dois números é: ', resultado)
---
+++ Python 3 - Módulo 1/Desafio04.py
@@ -0,0 +1,11 @@
+'''
+Desafio 01 replicado com {} . format,
+que serve para substituir a formatação.
+Veja o exemplo abaixo:
+'''
+nome = input("Qual seu nome?: ")
+print ("Prazer em conhecer, {}!". format (nome))
+'''
+As chaves estão sendo substituídas pelo obejto
+nome.
+'''
